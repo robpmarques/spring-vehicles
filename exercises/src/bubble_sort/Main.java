687a9a358d73e811e9fc9c953638c0b560a18aab
Roberto <matsufrg@gmail.com>
2023-07-12 19:47:27 -0300

    first commit to exercises

---
+++ exercises/src/bubble_sort/Main.java
@@ -0,0 +1,21 @@
+package bubble_sort;
+
+import election.Election;
+import election.ElectionReport;
+
+import java.util.Arrays;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 1, 2, 3};
+
+        BubbleSort bubbleSort = new BubbleSort();
+
+        bubbleSort.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
