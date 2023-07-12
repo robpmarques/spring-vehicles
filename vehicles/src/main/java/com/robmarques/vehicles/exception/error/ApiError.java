95373f2b044ffa8351c42d8cabef3ad1ee35fcfb
Roberto <matsufrg@gmail.com>
2023-07-12 19:51:11 -0300

    fix: removing unused imports

--- vehicles/src/main/java/com/robmarques/vehicles/exception/error/ApiError.java
+++ vehicles/src/main/java/com/robmarques/vehicles/exception/error/ApiError.java
@@ -1,12 +1,7 @@
 package com.robmarques.vehicles.exception.error;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
-
-import java.time.LocalDateTime;
 
 @Data
 public class ApiError {
